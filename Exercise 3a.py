ae927bd756c1f42ad5c23d2c299bc41455f6ed7a
viby <mathys@colorado.edu>
2026-07-02 15:11:56 -0600

    Added Channel class to Exercise 3a

--- Exercise 3a.py
+++ Exercise 3a.py
@@ -1,7 +1,6 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -26,9 +25,12 @@
 
 # %%
 import numpy as np
+rng = np.random.default_rng()
 
 
 # %% [markdown]
+# ## Source Class
+#
 # Create:
 #
 # ```python
@@ -45,6 +47,8 @@ import numpy as np
 # Methods:
 #
 # ```python
+# set_text()
+# set_LSBfirst()
 # generate()
 # ```
 #
@@ -53,10 +57,17 @@ import numpy as np
 class Source:
 
     def __init__(self, text, LSBfirst=1):
+        if len(text) < 1:
+            raise ValueError("text must have length > 1")
         self.text = text
         self.LSBfirst = LSBfirst
-        self.bits = None
+        
+    def set_text(self, text):
+        self.text = text
 
+    def set_LSBfirst(self, LSBfirst):
+        self.LSBfirst = LSBfirst
+    
     def generate(self):
         bitsperchar = 8
         textnum = np.array([ord(c) for c in self.text])  # convert ASCII to numeric
@@ -65,14 +76,144 @@ class Source:
         else:
             p2 = np.power(2.0, 1 + np.arange(-bitsperchar, 0))
         B = np.array(np.mod(np.array(np.floor(np.outer(textnum, p2)), int), 2), np.int8)
-        self.bits = np.reshape(B, B.size)
-        return self.bits
+        bits = np.reshape(B, B.size)
+        return bits
         
 
 
 # %%
 src = Source("Hi")
 bits = src.generate()
+print(src.LSBfirst)
 print(bits)
+
+# %%
+src.set_LSBfirst(0)
+bits = src.generate()
+print(src.LSBfirst)
+print(bits)
+
+
+# %%
+#src.set_text("Bye!")
+#src.set_LSBfirst(1)
+#bits = src.generate()
+#print(src.LSBfirst)
+#print(bits)
+
+# %% [markdown]
+# ## Receiver Class
+#
+# Create:
+#
+# ```python
+# class Receiver:
+# ```
+#
+# Internal state:
+#
+# ```python
+# bits
+# decoded
+# LSBfirst
+# ```
+#
+# Methods:
+#
+# ```python
+# BER()
+# received_text()
+# ```
+#
+
+# %%
+class Receiver:
+
+    def __init__(self, bits, decoded, LSBfirst=1):
+        self.bits = bits
+        self.decoded = decoded
+        self.LSBfirst = LSBfirst
+        
+    def BER(self):    # bit error rate (fraction of incorrectly decoded bits)
+        L = min(len(self.bits), len(self.decoded))
+        BER = np.where(self.bits!=self.decoded)[0].size/L
+        return BER
+
+    def received_text(self):
+        bitsperchar = 8
+        dnb = self.decoded[0:bitsperchar*int(len(self.decoded)/bitsperchar)]
+                         # make multiple of bitsperchar long
+        B = np.array(np.reshape(dnb, (int(len(dnb)/bitsperchar), bitsperchar)), int)
+        if self.LSBfirst == 1:
+            p2 = np.power(2, np.arange(0, bitsperchar))
+        else:
+            p2 = np.power(2, np.arange(bitsperchar, 0, -1) - 1)
+        textnum = np.array(np.dot(B, p2))
+        textrx = ''.join(chr(n) for n in textnum)
+        return textrx
+
+
+
+# %%
+decoded = src.generate()
+decoded[3] = np.mod(1+decoded[3],2) 
+rx = Receiver(src.generate(), decoded, src.LSBfirst)
+
+# %%
+print(rx.BER())
+print(rx.received_text())
+
+
+# %% [markdown]
+# ## Channel Class
+#
+# Create:
+#
+# ```python
+# class Channel:
+# ```
+#
+# Internal state:
+#
+# ```python
+# PbE    # Probability of bit error
+# ```
+#
+# Methods:
+#
+# ```python
+# transmit()
+# get_PbE()
+# ```
+#
+
+# %%
+class Channel:
+
+    def __init__(self, PbE):
+        if (PbE < 0) or (PbE > 1):
+            raise ValueError("PbE must be in range 0...1.0")
+        self.PbE = PbE
+
+    def get_PbE(self):
+        return self.PbE
+
+    def transmit(self, binary_in):
+        L = len(binary_in)
+        ix = np.where(rng.random(L) <= self.PbE)
+        out = binary_in.copy()
+        out[ix] = np.mod(out[ix] + 1, 2)   # flip error bits
+        return out
+
+
+
+# %%
+src = Source("The quick brown fox jumps over the lazy dog 0123456789")
+ch = Channel(0.01)
+decoded = ch.transmit(src.generate())
+rx = Receiver(src.generate(), decoded, src.LSBfirst)
+print(f'BER: {rx.BER()}')
+print(rx.received_text())
+
 
 # %%
